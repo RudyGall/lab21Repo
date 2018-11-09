8b801ded28e72a76e88eb4eecc9a3fa9212e8d79
Rudy <rudyegall@gmail.com>
2018-11-08 19:38:17 -0500

    second commit

--- startCoffeeShopLab/Models/UserInfo.cs
+++ startCoffeeShopLab/Models/UserInfo.cs
@@ -15,14 +15,20 @@ namespace startCoffeeShopLab.Models
             Email = "";
             PhoneNumber = "";
             Password = "";
+            Date = "";
+            Age = "";
+            Human = "";
         }
-        public UserInfo(string firstName, string lastName, string email, string phoneNumber, string password)
+        public UserInfo(string firstName, string lastName, string email, string phoneNumber, string password, string date, string age, string human)
         {
             FirstName = firstName;
             LastName = lastName;
             Email = email;
             PhoneNumber = phoneNumber;
             Password = password;
+            Date = date;
+            Age = age;
+            Human = human;
         }
             [Required]
             [RegularExpression("^[a-zA-Z]{2,}$")]
@@ -43,5 +49,17 @@ namespace startCoffeeShopLab.Models
             [Required]
             [RegularExpression(@"^[a-zA-Z]\w{3,14}$")]
             public string Password { set; get; }
+
+            [Required]
+            [RegularExpression(@"^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$")]
+            public string Date { set; get; }
+
+            [Required]
+            [RegularExpression(@"^$")]
+            public string Age { set; get; }
+
+            [Required]
+            [RegularExpression(@"^$")]
+            public string Human { set; get; }
     }
     }
--- startCoffeeShopLab/Scripts/formValidation.js
+++ startCoffeeShopLab/Scripts/formValidation.js
@@ -1,1 +1,89 @@
-﻿
+﻿var namePattern = /^[a-zA-Z]{2,}$/
+var ValidateFirstName = function () {
+    if (namePattern.test(document.getElementById("FN").value)) {
+        document.getElementById("message").style.color = "green"
+        document.getElementById("message").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message").innerHTML = "Invalid"
+        document.getElementById("message").style.color = "red"
+    }
+}
+var namePattern = /^[a-zA-Z]{2,}$/
+var ValidateLastName = function () {
+    if (namePattern.test(document.getElementById("LN").value)) {
+        document.getElementById("message2").style.color = "green"
+        document.getElementById("message2").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message2").innerHTML = "Invalid"
+        document.getElementById("message2").style.color = "red"
+    }
+}
+var emailPattern = /^[A-z0-9]{5,30}(@)(.+){5,10}$/
+var ValidateEmail = function () {
+    if (emailPattern.test(document.getElementById("EM").value)) {
+        document.getElementById("message3").style.color = "green"
+        document.getElementById("message3").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message3").innerHTML = "Invalid"
+        document.getElementById("message3").style.color = "red"
+    }
+}
+var phonePattern = /^\d{10}$/
+var ValidatePhoneNumber = function () {
+    if (phonePattern.test(document.getElementById("PN").value)) {
+        document.getElementById("message4").style.color = "green"
+        document.getElementById("message4").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message4").innerHTML = "Invalid"
+        document.getElementById("message4").style.color = "red"
+    }
+}
+var passwordPattern = /^[a-zA-Z]\w{3,14}$/
+var ValidatePassword = function () {
+    if (passwordPattern.test(document.getElementById("PWD").value)) {
+        document.getElementById("message5").style.color = "green"
+        document.getElementById("message5").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message5").innerHTML = "Invalid"
+        document.getElementById("message5").style.color = "red"
+    }
+}
+var datePattern = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$/
+var ValiDate = function () {
+    if (datePattern.test(document.getElementById("DT").value)) {
+        document.getElementById("message6").style.color = "green"
+        document.getElementById("message6").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message6").innerHTML = "Invalid"
+        document.getElementById("message6").style.color = "red"
+    }
+}
+var agePattern = /^$/
+var ValidateAge = function () {
+    if (agePattern.test(document.getElementById("AGE").value)) {
+        document.getElementById("message7").style.color = "green"
+        document.getElementById("message7").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message7").innerHTML = "Invalid"
+        document.getElementById("message7").style.color = "red"
+    }
+}
+var humanPattern = /^$/
+var ValidateHuman = function () {
+    if (humanPattern.test(document.getElementById("HMN").value)) {
+        document.getElementById("message8").style.color = "green"
+        document.getElementById("message8").innerHTML = "Valid"
+    }
+    else {
+        document.getElementById("message8").innerHTML = "Invalid"
+        document.getElementById("message8").style.color = "red"
+    }
+}
+
